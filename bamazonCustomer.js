ebbb3bc1d5e0029635653e6f7f6701f3c06cae48
Andrea Velasquez <velasquez189@gmail.com>
2018-05-05 02:37:19 -0400

    Talbe is working; table substracts user input

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,69 +2,73 @@ var mysql = require("mysql");
 var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
+  host: "localhost",
+  port: 3306,
 
-    user: "root",
+  user: "root",
 
-    password: "16011989",
-    database: "bamazonDB"
+  password: "16011989",
+  database: "bamazonDB"
 });
 
 connection.connect(function (err) {
-    if (err) throw err;
-    console.log("Connected to" + connection.threadId + "\n");
-    start();
-    // connection.end()
+  if (err) throw err;
+  console.log("Connected to" + connection.threadId + "\n");
+  start();
+  // connection.end()
 });
 
 //Display Table with IDs, Product Name, and Pricing
 function showItems() {
-    connection.query("SELECT * FROM products", function (err, res) {
-        for (var i = 0; i < res.length; i++) {
-            console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].price);
-        }
-        console.log("\n-----------------------------------\n");
-    });
+  connection.query("SELECT * FROM products", function (err, res) {
+    for (var i = 0; i < res.length; i++) {
+      console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].price + " | " + res[i].stock_quantity);
+    }
+    console.log("\n-----------------------------------\n");
+  });
 }
 
 //Displays the questions of Item ID and 
 function start() {
-    showItems();
-    connection.query("SELECT * FROM products", function (err, res) {
-        if (err) throw err;
-        inquirer.prompt([{
-                    name: "choice",
-                    type: "input",
-                    message: "Do you know the product ID of your item?",
-                },
-                {
-                    name: "quantity",
-                    type: "input",
-                    message: "How many would you like to purchase?"
-                }
-            ])
-            .then(function (answer) {
-                var selection = answer.choice - 1;
-                if (answer.quantity > res[selection].stock_quantity) {
-                    console.log("Too many ordered.");
-                } else {
-                    connection.query(
-                        "UPDATE products SET ? WHERE ?", [{
-                                stock_quantity: answer.quantity
-                            },
-                            {
-                                item_id: answer.choice
-                            }
-                        ],
-                        function (err) {
-                            if (err) throw err;
-                            console.log("New item(s) in your cart!");
-                            // start();
-                        }
-                    );
-                }
+  showItems();
+  connection.query("SELECT * FROM products", function (err, res) {
+    if (err) throw err;
+    inquirer.prompt([{
+          name: "choice",
+          type: "input",
+          message: "Do you know the product ID of your item?",
+        },
+        {
+          name: "quantity",
+          type: "input",
+          message: "How many would you like to purchase?"
+        }
+      ])
+      .then(function (answer) {
+        var selection = answer.choice - 1;
+        if (answer.quantity > res[selection].stock_quantity) {
+          console.log("Too many ordered.");
+        } else {
+          var newQuantity = res[selection].stock_quantity - answer.quantity;
+          var totalCost = res[selection].price * answer.quantity;
+          connection.query(
+            "UPDATE products SET ? WHERE ?", [{
+                stock_quantity: newQuantity
+              },
+              {
+                item_id: answer.choice
+              }
+            ],
+            function (err) {
+              // if (err) throw err;
+              console.log("New item(s) in your cart!");
+              console.log("Your total cost is $" + totalCost);
+              console.log("\n")
+              connection.end()
+            }
+          );
+        }
 
-            })
-    });
+      })
+  });
 }
